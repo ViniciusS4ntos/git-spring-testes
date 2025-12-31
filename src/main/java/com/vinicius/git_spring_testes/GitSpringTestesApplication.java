b2e89b1352f436c98a8bd0dddb2f13d4c6035d93
ViniciusS4ntos <edsonklibrsouza2015@gmail.com>
2025-12-31 16:10:09 -0300

    #1 iniciar repo

---
+++ src/main/java/com/vinicius/git_spring_testes/GitSpringTestesApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.git_spring_testes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringTestesApplication.class, args);
+	}
+
+}
